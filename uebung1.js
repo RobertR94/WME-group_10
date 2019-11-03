441a967c0f05ccbc4e3f765f18de3af6d72db887
RobertR94 <roberto_r@outlook.de>
2019-11-03 19:30:08 +0100

    table style + sort

--- uebung1.js
+++ uebung1.js
@@ -3,6 +3,17 @@ window.onscroll = function() {
   myFunction();
 };
 
+//Read the data fom the csv file
+//returns the data as a text in a string
+function readDataAsTxt(fileName) {
+  
+
+  var reader = new FileReader();
+  
+  return reader.readAsText(fileName).result;
+      
+}
+
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
@@ -17,3 +28,67 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+//sort table
+function sortCountry(n){
+
+  var table, dir, run, shouldSwitch, cell, elem, nextElem;
+  table = document.getElementById('world_data');
+  run = true;
+  cell = 1;
+
+  if(n == 0){
+    dir = "asc";
+  }
+  else{
+    dir="desc";
+  }
+
+  
+  while(run){
+
+    run = false;
+
+    for(i= 1; i < (table.rows.length - 1); i++){
+
+      elem = table.rows[i].cells[cell];
+      nextElem = table.rows[i + 1].cells[cell];
+  
+      if(dir == "asc"){
+        if(elem.innerHTML.toLowerCase() > nextElem.innerHTML.toLowerCase()){
+          table.rows[i].parentNode.insertBefore(table.rows[i + 1], table.rows[i]);
+          run = true;
+        }
+      }
+      else if(dir == "desc"){
+        if(elem.innerHTML.toLowerCase() < nextElem.innerHTML.toLowerCase()){
+          table.rows[i].parentNode.insertBefore(table.rows[i + 1], table.rows[i]);
+          run = true;
+        }
+      }
+    }
+  }
+
+}
+
+/*
+while(run){
+
+    run = false;
+
+    for(i= 1; i < (table.rows.length -1); i++){
+
+      shouldSwitch = false;
+      elem = table.rows[i].cells[cell];
+      nextElem = table.rows[i + 1].cells[cell];
+
+      if(dir == "asc"){
+
+      }
+      else if(dir == "desc"){
+        
+      }
+    }
+  }
+
+*/
